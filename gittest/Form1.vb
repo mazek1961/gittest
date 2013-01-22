67d3874806b07515bcbd5e3ade2f3a7eec55376c
Mike Elberg <mike@nugs.net>
2013-01-22 14:40:06 -0800

    test 3

--- gittest/Form1.vb
+++ gittest/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim smock As String
+
     End Sub
 End Class
